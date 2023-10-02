fb75c947888d1d8faed637a6af84633147c595dc
rafael-lage <90780368+rafael-lage@users.noreply.github.com>
2023-10-02 10:51:24 +0000

    Criando uma thread e executando o codigo

--- maze_runner.cpp
+++ maze_runner.cpp
@@ -8,6 +8,7 @@
 #include <string>
 #include <vector>
 
+
 // Matriz de char representnado o labirinto
 char** maze; // Voce também pode representar o labirinto como um vetor de vetores de char (vector<vector<char>>)
 
@@ -122,6 +123,7 @@ bool checkPos(pos_t pos){
 	return(false);
 }
 
+bool exit_found = 0;
 // Função responsável pela navegação.
 // Recebe como entrada a posição initial e retorna um booleando indicando se a saída foi encontrada
 bool isFirst = true;
@@ -134,6 +136,8 @@ bool walk(pos_t pos) {
 		// Imprime o labirinto
 		//printf("isFirst = %d\n",isFirst);
 	pos_t currentPos;
+	
+	
 	if(isFirst){
 		currentPos.i = pos.i;
 		currentPos.j = pos.j;
@@ -143,6 +147,7 @@ bool walk(pos_t pos) {
 		currentPos = valid_positions.top();
 		valid_positions.pop();
 	}
+	
 	maze[currentPos.i][currentPos.j] = '.';
 	system("clear");
 	print_maze();
@@ -161,49 +166,39 @@ bool walk(pos_t pos) {
 	downPos.i = currentPos.i+1;
 	downPos.j = currentPos.j;
 
+	//Adiciona os valores ao redor no vetor de posicoes
 	if(!isEnd) isEnd = checkPos(rightPos);
 	if(!isEnd) isEnd = checkPos(leftPos);
 	if(!isEnd) isEnd = checkPos(upPos);
 	if(!isEnd) isEnd = checkPos(downPos);
 	
-	return (isEnd);
-		/* Dado a posição atual, verifica quais sao as próximas posições válidas
-			Checar se as posições abaixo são validas (i>0, i<num_rows, j>0, j <num_cols)
-		 	e se são posições ainda não visitadas (ou seja, caracter 'x') e inserir
-		 	cada uma delas no vetor valid_positions
-		 		- pos.i, pos.j+1
-		 		- pos.i, pos.j-1
-		 		- pos.i+1, pos.j
-		 		- pos.i-1, pos.j
-		 	Caso alguma das posiçÕes validas seja igual a 's', retornar verdadeiro
-	 	*/
+	exit_found = isEnd;
 
-		
-	
-		// Verifica se a pilha de posições nao esta vazia 
-		//Caso não esteja, pegar o primeiro valor de  valid_positions, remove-lo e chamar a funçao walk com esse valor
-		// Caso contrario, retornar falso
-		/*
-		if (!valid_positions.empty()) {
-			pos_t next_position = valid_positions.top();
-			valid_positions.pop();
-		}*/
+	return (isEnd);
+
 }
+
+
 
 int main(int argc, char* argv[]) {
 	// carregar o labirinto com o nome do arquivo recebido como argumento
-	//pos_t initial_pos = load_maze("../data/maze7.txt");
-	pos_t initial_pos = load_maze(argv[1]);
+	pos_t initial_pos = load_maze("../data/maze7.txt");
+	//pos_t initial_pos = load_maze(argv[1]);
 	// chamar a função de navegação
-	bool exit_found = walk(initial_pos);
+	//bool exit_found = walk(initial_pos);
+	
 	while(!exit_found){
-		exit_found = walk(initial_pos);
+		//criar uma thread
+		//executa o walk comecando da posicao da anterior
+		std::thread walker(walk, initial_pos);
+		
+		walker.join();
+		//walker.detach();
 		if(!exit_found && valid_positions.empty()){
 			printf("\nNao possui saida\n");
 			return(0);
 		}
 	}
-	
 
 	for (int i = 0; i < num_rows; i++) {
     	delete[] maze[i];
